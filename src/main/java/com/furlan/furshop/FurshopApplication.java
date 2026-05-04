f1b1053f4fa908a4bf3c6f1e45c892e43db9e4d3
Rafael Medeiros Furlan <rafaelfurlan45@yahoo.com>
2026-05-04 13:02:54 -0300

    Project created

---
+++ src/main/java/com/furlan/furshop/FurshopApplication.java
@@ -0,0 +1,13 @@
+package com.furlan.furshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurshopApplication.class, args);
+	}
+
+}
