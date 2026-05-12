11a5c02fc224eb3f92d324355e74f0d7437d6e30
Rafael Medeiros Furlan <rafaelfurlan45@yahoo.com>
2026-05-12 11:24:09 -0300

    Category

--- src/main/java/com/furlan/furshop/config/TestConfig.java
+++ src/main/java/com/furlan/furshop/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.furlan.furshop.config;
 
+import com.furlan.furshop.entities.Category;
 import com.furlan.furshop.entities.Order;
 import com.furlan.furshop.entities.User;
 import com.furlan.furshop.entities.enums.OrderStatus;
+import com.furlan.furshop.repositories.CategoryRepository;
 import com.furlan.furshop.repositories.OrderRepository;
 import com.furlan.furshop.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +25,18 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Games");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
---
+++ src/main/java/com/furlan/furshop/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.furlan.furshop.repositories;
+
+import com.furlan.furshop.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+
+
+}
