3ca15b131ee3013f50f9f48b0429f660303771d8
Rafael Medeiros Furlan <rafaelfurlan45@yahoo.com>
2026-05-13 12:54:38 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/furlan/furshop/entities/OrderItem.java
+++ src/main/java/com/furlan/furshop/entities/OrderItem.java
@@ -42,6 +42,7 @@ public class OrderItem implements Serializable {
     public Product getProduct(){
         return id.getProduct();
     }
+
     public void setProduct(Product product){
         id.setProduct(product);
     }
