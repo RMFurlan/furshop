2b983a187dfb494cfae335816fcd627455038f52
Rafael Medeiros Furlan <rafaelfurlan45@yahoo.com>
2026-05-18 12:49:48 -0300

    Exception handling

---
+++ src/main/java/com/furlan/furshop/services/exceptions/DataBaseException.java
@@ -0,0 +1,9 @@
+package com.furlan.furshop.services.exceptions;
+
+public class DataBaseException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public DataBaseException(String msg){
+        super(msg);
+    }
+}
